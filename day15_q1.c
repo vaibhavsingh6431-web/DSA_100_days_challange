5950fb9199d1c4dd82bc03b6064a7003f1e1f138
vaibhavsingh6431-web <vaibhavsingh6431@gmail.com>
2026-02-24 00:27:27 +0530

    day14

---
+++ day15_q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int m ,n;
+    int matrix[100][100];
+    scanf("%d %d",&m,&n);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    int sum=0;
+    int minDim=m<n ? m:n;
+    for(int i=0;i<minDim;i++){
+        sum+=matrix[i][i];
+    }
+    printf("%d\n",sum);
+    return 0;
+}
